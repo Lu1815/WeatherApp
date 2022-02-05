204327bb6407c255a87633f7ee1937e01c6bb8c6
franklingonzalez1420 <87407849+franklingonzalez1420@users.noreply.github.com>
2022-02-05 11:01:50 -0600

    Corrected blank page on first load

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import Menu from './Components/topMenuBar'
-import Footer from './Components/footer'
-import './App.css';
+import Menu from "./Components/topMenuBar";
+import Footer from "./Components/footer";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LocalW from "./Components/localWeatherSquare";
+import SearchBox from "./Components/searchBox";
 // import SearchBox from './Components/searchBox'
 // import Index from './index.js'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Menu/>
-      </header>
-      <footer>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Menu />
+        </header>
+
+        <Routes>
+          <Route exact path="/" element={<LocalW />} />
+          <Route exact path="/WeatherApp" element={<LocalW />} />
+          <Route exact path="/localWeatherSquare" element={<LocalW />} />
+          <Route exact path="/searchBox" element={<SearchBox />} />
+        </Routes>
+
+        <footer></footer>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/topMenuBar.jsx
+++ src/Components/topMenuBar.jsx
@@ -1,29 +1,32 @@
-import Logo from '../img/SimpleWeatherLogo.png'
-import LocalW from '../Components/localWeatherSquare'
-import SearchBox from '../Components/searchBox'
-import { BrowserRouter as Router, Routes as Switch, Route, NavLink } from 'react-router-dom';
-import bars from '../img/burger-bar.png'
-import '../utilities/menuOptions'
+import Logo from "../img/SimpleWeatherLogo.png";
+import LocalW from "../Components/localWeatherSquare";
+import SearchBox from "../Components/searchBox";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import bars from "../img/burger-bar.png";
+import "../utilities/menuOptions";
 
-export default function Menu () {
-    return (        
-        <Router>
-            <nav>
-                <img className="logo" src={Logo} alt="logo"/>
-                <input type="checkbox" id="check"/>
-                <label htmlFor="check" className="checkbtn" id="menu-toggle">
-                    <img src={bars} alt="check" className="menubars"/>
-                </label>
-                <ul className="options" id="nav-menu">
-                    <li><NavLink to="/localWeatherSquare" className="btn">Home</NavLink></li>
-                    <li><NavLink to="/searchBox" className="btn">Search</NavLink></li>
-                </ul>
-            </nav>
-
-            <Switch>
-                <Route exact path="/localWeatherSquare" element={<LocalW/>}/>
-                <Route exact path="/searchBox" element={<SearchBox/>}/>
-            </Switch>
-        </Router>
-    );
+export default function Menu() {
+  return (
+    <>
+      <nav>
+        <img className="logo" src={Logo} alt="logo" />
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="checkbtn" id="menu-toggle">
+          <img src={bars} alt="check" className="menubars" />
+        </label>
+        <ul className="options" id="nav-menu">
+          <li>
+            <Link to="/localWeatherSquare" className="btn">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/searchBox" className="btn">
+              Search
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
